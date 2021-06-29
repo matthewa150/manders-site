5c81df305c733bdddbaae3ad872a9507e9df5750
Matthew Anderson <matthewa150@gmail.com>
2021-06-29 17:18:47 -0500

    Added parallax component

---
+++ components/ParallaxSection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="parallax-bg">
+    <div class="parallax-haze" />
+  </div>
+</template>
+
+<style>
+.parallax-bg {
+  background-image: url('/top-banner.jpg');
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /*filter: blur(8px) saturate(30%);
+  -webkit-filter: blur(8px) saturate(30%);*/
+
+  position: relative;
+  padding: 8rem 0;
+}
+
+.parallax-haze {
+  background-color: #be3131;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.7;
+}
+</style>
