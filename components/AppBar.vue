33c42e5295a2d28e25bcdfa2463f4e532a1454d6
Matthew Anderson <matthewa150@gmail.com>
2021-07-01 23:37:21 -0500

    Navbar collapse added

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,15 +1,18 @@
 <template>
-  <b-navbar :fixed="fixedTo" :class="customStyle" type="dark">
+  <b-navbar toggleable="lg" :fixed="fixedTo" :class="customStyle" type="dark">
     <b-container>
       <b-navbar-brand>
         <span class="font-weight-bold">Matthew</span>
         <span class="font-weight-light">Anderson</span>
       </b-navbar-brand>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="https://github.com/matthewa150">GitHub</b-nav-item>
-        <b-nav-item href="https://www.linkedin.com/in/matthew-anderson-a65063148">LinkedIn</b-nav-item>
-        <b-nav-item href="manders_cv.pdf" target="_blank">My CV</b-nav-item>
-      </b-navbar-nav>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="https://github.com/matthewa150">GitHub</b-nav-item>
+          <b-nav-item href="https://www.linkedin.com/in/matthew-anderson-a65063148">LinkedIn</b-nav-item>
+          <b-nav-item href="manders_cv.pdf" target="_blank">My CV</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-container>
   </b-navbar>
 </template>
